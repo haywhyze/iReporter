c858424c89050ff67d2e8e50097c3300941cf108
Yusuf Ayo <haywhyze@gmail.com>
2018-11-29 22:32:29 +0100

    ft(get specific red-flag record):
    - Updated RedFlagController class to implement the get specific red-flag record.
    - Updated the server file and routes handler to effect the changes.
    - Made changes to the test suite to correct an assertion error on the getOne method.
    - Created a helper function to validate the req.params.id
    [finishes #162196028]

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,7 @@ import RedFlagController from '../controllers/red-flag';
 const router = Router();
 
 router.get('/api/v1/red-flags', RedFlagController.getAll);
-// router.get('/api/v1/red-flags/:id', RedFlagController.getOne);
+router.get('/api/v1/red-flags/:id', RedFlagController.getOne);
 // router.post('/api/v1/red-flags', RedFlagController.create);
 // router.patch('/api/v1/red-flags/:id/location', RedFlagController.updateLocation);
 // router.patch('/api/v1/red-flags/:id/comment', RedFlagController.updateComment);
