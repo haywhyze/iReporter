d786454dd675ff85e465da5eefba9c80da99ff18
Yusuf Ayo <haywhyze@gmail.com>
2018-12-05 06:57:54 +0100

    chore(template): implement feedback on ui
    - make go to top icon visible when footer is in view
    - make card components more opaque
    - merge update and delete reports into one page
    - add map to update report modal page
    - make links in footer just links and not button
    - align footer contents
    - align navbar icons with page content
    - align buttons for rejecting/resolving as admin in a row
    - make better color representation to improve ui
    
    [Delivers #162394843]

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,5 +1,8 @@
 let tablinks = document.querySelectorAll('.tablinks');
 let tabcontent = document.querySelectorAll('.tabcontent');
+const nav_ul = document.querySelector('.site-nav-ul');
+const logo = document.querySelector('.site-nav__logo');
+const site_nav = document.querySelector('.site-nav');
 
 tabcontent = Array.from(tabcontent);
 tablinks = Array.from(tablinks);
@@ -19,3 +22,18 @@ let openTab = () => {
 
 
 tablinks.map((e) => e.addEventListener('click', openTab()));
+
+// TO animate page header on scroll
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      nav_ul.style.padding = '0';
+      logo.setAttribute('src', 'img/logo-ireporter2.svg');
+      logo.style.height = '3em';
+      site_nav.style.backgroundColor = 'rgb(32, 32, 32)';
+    } else {
+      nav_ul.style.padding = '0.5em';
+      logo.setAttribute('src', 'img/logo.svg');
+      logo.style.height = '4em';
+      site_nav.style.backgroundColor = 'rgba(32, 32, 32, .8)';
+    }
+  });
--- UI/js/script.js
+++ UI/js/script.js
@@ -1,25 +1,27 @@
-let hero = document.querySelector('.hero');
-let header = document.querySelector('header');
-let nav_ul = document.querySelector('.site-nav-ul');
-let logo = document.querySelector('.site-nav__logo');
-let site_nav = document.querySelector('.site-nav');
+/* eslint-disable */
+
+const hero = document.querySelector('.hero');
+// const header = document.querySelector('header');
+const nav_ul = document.querySelector('.site-nav-ul');
+const logo = document.querySelector('.site-nav__logo');
+const site_nav = document.querySelector('.site-nav');
 
 let tablinks = document.querySelectorAll('.tablinks');
 let tabcontent = document.querySelectorAll('.tabcontent');
 
 // To change backround image on home screen
-let b_urls = [`
-    linear-gradient( 
+const backgroundUrl = [`
+linear-gradient( 
         to left bottom, 
         rgba(16, 42, 110, .6), 
         rgba(242, 48, 62, .6)),
         url('img/uganda.jpg')`,
-    `linear-gradient( 
+`linear-gradient( 
         to right bottom, 
         rgba(16, 42, 110, .6), 
         rgba(242, 48, 62, .6)),
         url('img/images.jpeg')`,
-    `linear-gradient( 
+`linear-gradient( 
         to left top, 
         rgba(16, 42, 110, .6), 
         rgba(242, 48, 62, .6)), 
@@ -28,109 +30,123 @@ let b_urls = [`
         to left top, 
         rgba(16, 42, 110, .6), 
         rgba(242, 48, 62, .6)), 
-        url('img/corruption.jpg')`
-]
+        url('img/corruption.jpg')`,
+];
 let initial = 0;
-let changeBackground = () => {
-    hero.style.backgroundImage = b_urls[initial = ++initial % b_urls.length];
-    hero.style.transition = 'background 1s ease-in-out';
-}
+const changeBackground = () => {
+  hero.style.backgroundImage = backgroundUrl[initial += 1 % backgroundUrl.length];
+  hero.style.transition = 'background 1s ease-in-out';
+};
 if (hero) setInterval(changeBackground, 5000);
 
 
 // TO animate page header on scroll
 window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        nav_ul.style.padding = '0';
-        logo.setAttribute('src', 'img/logo-ireporter2.svg');
-        logo.style.height = '3em';
-        site_nav.style.backgroundColor = 'rgb(32, 32, 32)';
-    } else {
-        nav_ul.style.padding = '0.5em';
-        logo.setAttribute('src', 'img/logo.svg');
-        logo.style.height = '4em';
-        site_nav.style.backgroundColor = 'rgba(32, 32, 32, .8)';
-    }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    nav_ul.style.padding = '0';
+    logo.setAttribute('src', 'img/logo-ireporter2.svg');
+    logo.style.height = '3em';
+    site_nav.style.backgroundColor = 'rgb(32, 32, 32)';
+  } else {
+    nav_ul.style.padding = '0.5em';
+    logo.setAttribute('src', 'img/logo.svg');
+    logo.style.height = '4em';
+    site_nav.style.backgroundColor = 'rgba(32, 32, 32, .8)';
+  }
 });
 
 // For Tab functionality
 tabcontent = Array.from(tabcontent);
 tablinks = Array.from(tablinks);
 let openTab = () => {
-    tablinks.map((e) => {
-        // if (e.classList.contains('active')) e.classList.remove('active');
-        e.addEventListener('click', () => {
-            tablinks.map((x) => {
-                if (x.classList.contains('active')) x.classList.remove('active');
-            })
-            e.classList.add('active');
-            tabcontent.map((e) => e.style.display = 'none');
-            document.querySelector(`#${e.dataset.tab}`).style.display = 'block';
-        })
-    })
-}
+  tablinks.map((e) => {
+    // if (e.classList.contains('active')) e.classList.remove('active');
+    e.addEventListener('click', () => {
+      tablinks.map((x) => {
+        if (x.classList.contains('active')) x.classList.remove('active');
+      });
+      e.classList.add('active');
+      tabcontent.map(e => e.style.display = 'none');
+      document.querySelector(`#${e.dataset.tab}`).style.display = 'block';
+    });
+  });
+};
 
 
-tablinks.map((e) => e.addEventListener('click', openTab()));
+tablinks.map(e => e.addEventListener('click', openTab()));
 
-//Google Maps
+// Google Maps
 // For the functions on the map
-var markers = [];
+const markers = [];
 
 const clearMarkers = () => {
-    if (markers[0])
-        markers.map((e) => e.setMap(null));
-}
+  if (markers[0]) {
+    markers.map(e => e.setMap(null));
+  }
+};
 
 const placeMarkerAndPanTo = (latLng, map) => {
-    const marker = new google.maps.Marker({
-        position: latLng,
-        map: map
-    });
-    map.panTo(latLng);
-    markers.push(marker);
-}
+  const marker = new google.maps.Marker({
+    position: latLng,
+    map,
+  });
+  map.panTo(latLng);
+  markers.push(marker);
+};
 
 function initMap() {
-
-    var map = new google.maps.Map(document.querySelector('#map'), {
-        zoom: 13,
-        center: {
-            lat: 6.605874,
-            lng: 3.349149
-        }
+  let map = document.querySelectorAll('.map');
+  map = Array.from(map);
+  let a;
+  map = map.map((e) => {
+    e = new google.maps.Map(e, {
+      zoom: 13,
+      center: {
+        lat: 6.605874,
+        lng: 3.349149,
+      },
     });
 
+    e.addListener('click', (x) => {
+      clearMarkers();
+      placeMarkerAndPanTo(x.latLng, e);
+      geocodeLatLng(e.latLng);
+    })
+  });
 
-    let location = document.querySelector('#location');
-    let address = document.querySelector('#address');
-    map.addListener('click', (e) => {
-        clearMarkers();
-        placeMarkerAndPanTo(e.latLng, map);
-        geocodeLatLng(e.latLng);
-        location.value = `${e.latLng}`;
-    });
+  const location = document.querySelector('#location');
+  const address = document.querySelector('#address');
+  map.map((x) => {
+    // console.log(x)
+    // x.addListener('click', (e) => {
+    //   console.log(e)
+    //   
+    //   // placeMarkerAndPanTo(e.latLng, x);
+    //   // geocodeLatLng(e.latLng);
+    //   // location.value = `${e.latLng}`;
+    // });
+  })
 }
 
 
 
 function geocodeLatLng(latLng) {
-    const geocoder = new google.maps.Geocoder;
-    var latlngStr = String(latLng).split(',', 2);
+  const geocoder = new google.maps.Geocoder;
+  var latlngStr = String(latLng).split(',', 2);
 
-    var latlng = { lat: parseFloat(latlngStr[0].substr(1)), lng: parseFloat(latlngStr[1]) };
+  var latlng = { lat: parseFloat(latlngStr[0].substr(1)), lng: parseFloat(latlngStr[1]) };
 
-    geocoder.geocode({ 'location': latlng }, function(results, status) {
-        if (status === 'OK') {
-            if (results[0]) {
-                address.innerHTML = results[0].formatted_address;
-            } else {
-                address.innerHTML = 'Location Address not Available';
-            }
+  geocoder.geocode({ 'location': latlng }, function(results, status) {
+    if (status === 'OK') {
+        if (results[0]) {
+            address.innerHTML = results[0].formatted_address;
         } else {
-            console.log('Place not found');
+            address.innerHTML = 'Location Address not Available';
         }
-    });
+    } else {
+        console.log('Place not found');
+    }
+  });
 }
 
 
@@ -139,25 +155,24 @@ let updateBtn = document.querySelectorAll('.update-button');
 let modal = document.querySelectorAll('.modal');
 modal = Array.from(modal);
 modal.map((e) => {
-    e.style.display = 'none';
-    console.log(e);
+  e.style.display = 'none';
 })
-let me;
+let modalDiv;
 updateBtn = Array.from(updateBtn);
 updateBtn.map((e) => {
-    e.addEventListener('click', (x) => {
-        x.preventDefault();
-        e.nextElementSibling.style.display = 'block';
-        me = e.nextElementSibling;
-    })
+  e.addEventListener('click', (x) => {
+      x.preventDefault();
+      e.parentElement.nextElementSibling.style.display = 'block';
+      modalDiv = e.parentElement.nextElementSibling;
+  })
 })
 
 // When the user clicks on <span> (x), close the modal
 var span = document.getElementsByClassName("close");
 span = Array.from(span)
 span.map((e) => {
-    e.onclick = function() {
-        me.style.display = "none";
-    }
+  e.onclick = function() {
+      modalDiv.style.display = "none";
+  }
 
 })
